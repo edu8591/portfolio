a9c68bb92de2c33f279a7e4d55bfdff4752032d7
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-09-09 17:47:29 -0500

    create a grayscale hover image component

---
+++ src/components/grayscale-image.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+type GrayscaleImageProps = {
+  icon:
+    | "bootstrap"
+    | "css"
+    | "git"
+    | "github"
+    | "html"
+    | "nestjs"
+    | "nextjs"
+    | "node"
+    | "psql"
+    | "rails"
+    | "ruby"
+    | "react"
+    | "tailwind"
+    | "typescript";
+  height: number;
+  width: number;
+  className?: string;
+};
+
+export const GrayscaleImage = ({
+  icon,
+  height,
+  width,
+  className,
+}: GrayscaleImageProps) => {
+  return (
+    <Image
+      src={`/icons/${icon}.svg`}
+      height={height}
+      width={width}
+      alt={`${icon} logo`}
+      className={cn(
+        "grayscale hover:grayscale-0 transition duration-400 ease-in-out",
+        className ?? className
+      )}
+    />
+  );
+};
