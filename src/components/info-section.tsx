80dc7c36948e0fbbff6463d037db6edc5b267b43
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-09-10 10:48:01 -0500

    adds overflow-x-hidden

--- src/components/info-section.tsx
+++ src/components/info-section.tsx
@@ -4,7 +4,7 @@ import { Technologies } from "./technologies";
 
 export const InfoSection = () => {
   return (
-    <div className="md:h-[calc(100vh-20px)] md:overflow-y-scroll px-4 gap-y-6 pt-5">
+    <div className="md:h-[calc(100vh-20px)] md:overflow-y-scroll overflow-x-hidden px-4 gap-y-6 pt-5">
       <AboutMe />
       <Experience />
       <Technologies />
