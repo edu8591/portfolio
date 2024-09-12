2e4272ff4502c077a6353b316177d9b9216f91b9
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-09-12 10:58:37 -0500

    create the projects component

---
+++ src/constants/projects.ts
@@ -0,0 +1,22 @@
+import { Project } from "@/types/project";
+
+export const projects: Project[] = [
+  {
+    name: "Duolingo Clone - Lingo",
+    src: "/images/lingo.png",
+    description:
+      "Duolingo clone website build with Next.js,Tailwindcss, Neon for database, Clerkjs for auth and stripe for payments processing. Admin dashboard to create new courses and lessons",
+  },
+  {
+    name: "Omnifoods",
+    src: "/images/omnifoods.png",
+    description:
+      "Landing page for a food delivery service built with HTML, CSS, and JavaScript. Made to learn more about CSS and positionig elements with grid and flexbox also to learn about responsive design",
+  },
+  {
+    name: "POW checkout page",
+    src: "/images/POW.png",
+    description:
+      "Made a responsive checkout page with HTML, CSS and Javascript, used bootstrap to position elements and make the page responsive",
+  },
+];
---
+++ src/types/project.ts
@@ -0,0 +1,5 @@
+export type Project = {
+  name: string;
+  src: string;
+  description: string;
+};
