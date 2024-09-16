6b455eb2664992f66e26b09d50f5477604edca67
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-09-16 14:05:29 -0500

    increase paddingTop and adds removes scrollbar and implements smooth scroll

--- src/components/info-section.tsx
+++ src/components/info-section.tsx
@@ -5,7 +5,7 @@ import { Technologies } from "./technologies";
 
 export const InfoSection = () => {
   return (
-    <div className="md:h-[calc(100vh-20px)] md:overflow-y-scroll md:col-span-3 px-4 gap-y-6 pt-5">
+    <div className="md:h-[calc(100vh-20px)] md:overflow-y-scroll md:col-span-3 px-4 gap-y-6 pt-32 no-scrollbar scroll-smooth">
       <AboutMe />
       <Experience />
       <Technologies />
