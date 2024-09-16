1bd8982a39df1dbff7ed0ecb7b91efd49be5b263
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-09-16 14:07:33 -0500

    refactor component to use the new technologiesList constant

--- src/components/technologies.tsx
+++ src/components/technologies.tsx
@@ -7,6 +7,7 @@ import { Title } from "./title";
 import { motion } from "framer-motion";
 import { AnimateEntryIcon } from "./animate-entry-icon";
 import { useTranslations } from "next-intl";
+import { technologiesList } from "@/constants/technologies";
 
 export const Technologies = () => {
   const t = useTranslations("technologies");
@@ -16,7 +17,7 @@ export const Technologies = () => {
       <Title>{t("title")}</Title>
 
       <motion.div
-        className="grid grid-cols-3 gap-x-12 gap-y-4 md:grid-cols-4 md:gap-x-8 lg:grid-cols-5  lg:gap-6 place-items-center"
+        className="grid grid-cols-3 gap-x-12 gap-y-4  md:gap-x-8 lg:grid-cols-5 lg:gap-6 place-items-center"
         variants={{
           visible: { transition: { staggerChildren: 0.2 } },
           hidden: {},
@@ -25,81 +26,15 @@ export const Technologies = () => {
         whileInView="visible"
         initial="hidden"
       >
-        <AnimateEntryIcon>
-          <TooltipWrapper description="HTML5">
-            <GrayscaleImage icon="html" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Css">
-            <GrayscaleImage icon="css" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="React.js">
-            <GrayscaleImage icon="react" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Next.js">
-            <GrayscaleImage icon="nextjs" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Nest.js">
-            <GrayscaleImage icon="nestjs" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Node.js">
-            <GrayscaleImage icon="node" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Typescript">
-            <GrayscaleImage icon="typescript" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Ruby">
-            <GrayscaleImage icon="ruby" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Rails">
-            <GrayscaleImage icon="rails" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Tailwind">
-            <GrayscaleImage icon="tailwind" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Bootstrap">
-            <GrayscaleImage icon="bootstrap" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Git">
-            <GrayscaleImage icon="git" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Github">
-            <GrayscaleImage icon="github" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="PostgreSQL">
-            <GrayscaleImage icon="psql" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
-        <AnimateEntryIcon>
-          <TooltipWrapper description="Linux">
-            <GrayscaleImage icon="linux" height={60} width={60} />
-          </TooltipWrapper>
-        </AnimateEntryIcon>
+        {technologiesList.map(({ icon, description }) => {
+          return (
+            <AnimateEntryIcon key={icon + description}>
+              <TooltipWrapper description={description}>
+                <GrayscaleImage icon={icon} height={60} width={60} />
+              </TooltipWrapper>
+            </AnimateEntryIcon>
+          );
+        })}
       </motion.div>
     </section>
   );
