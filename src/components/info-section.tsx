81861ef3e3d2a91b3c5b19ba2b9e0b5d10755ac8
Eduardo Serrano <eduardoserrano8591@gmail.com>
2026-04-21 12:12:01 +0900

    Add Contact component to info section
    - Include Contact component after Projects section
    - Update spacing to lg:space-y-20 for consistency

--- src/components/info-section.tsx
+++ src/components/info-section.tsx
@@ -2,14 +2,17 @@ import { AboutMe } from "./about-me";
 import { Experience } from "./experience";
 import { Projects } from "./projects";
 import { Technologies } from "./technologies";
+// import { Contact } from "./contact";
 
 export const InfoSection = () => {
   return (
-    <div className="md:h-[calc(100vh-20px)] md:overflow-y-scroll md:col-span-3 px-4 gap-y-6 pt-32 no-scrollbar scroll-smooth">
+    <div className="space-y-16 lg:space-y-20">
       <AboutMe />
       <Experience />
       <Technologies />
       <Projects />
+      {/* <Contact /> */}
+      <div className="h-8"></div>
     </div>
   );
 };
