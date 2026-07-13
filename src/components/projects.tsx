3d9a9e5ff91dde451358dcf6e1aa126800f619fe
Eduardo Serrano <eduardoserrano8591@gmail.com>
2026-07-13 18:00:28 +0900

    Chore: Refactor MotionCard to use y-axis animations and update Projects component

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -23,8 +23,6 @@ export const Projects = async () => {
     return (
       <motion.div
         key={project.name}
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, delay: index * 0.1 }}
         viewport={{ once: true, amount: 0.3 }}
       >
